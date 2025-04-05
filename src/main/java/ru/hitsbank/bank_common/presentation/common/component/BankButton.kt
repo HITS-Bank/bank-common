d884e57656c27b70bcd67cbf6c09a90160968e42
Suslanium <zaharov22m@gmail.com>
2025-04-05 23:19:24 +0700

    Переводы, вынос в common и всякое разное

--- src/main/java/ru/hitsbank/bank_common/presentation/common/component/BankButton.kt
+++ src/main/java/ru/hitsbank/bank_common/presentation/common/component/BankButton.kt
@@ -1,6 +1,7 @@
 package ru.hitsbank.bank_common.presentation.common.component
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -46,6 +47,30 @@ object BankButton {
     }
 
     @Composable
+    fun VerticalRegular(
+        text: String,
+        onClick: () -> Unit,
+        colors: ButtonColors = ButtonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+            disabledContentColor = MaterialTheme.colorScheme.onPrimary,
+            disabledContainerColor = MaterialTheme.colorScheme.outline,
+        ),
+        icon: ImageVector? = null,
+        enabled: Boolean = true,
+        modifier: Modifier = Modifier,
+    ) {
+        VerticalButtonBase(
+            text = text,
+            onClick = onClick,
+            icon = icon,
+            enabled = enabled,
+            colors = colors,
+            modifier = modifier.height(40.dp),
+        )
+    }
+
+    @Composable
     fun Outlined(
         text: String,
         onClick: () -> Unit,
@@ -61,6 +86,32 @@ object BankButton {
         modifier: Modifier = Modifier,
     ) {
         OutlinedButtonBase(
+            text = text,
+            onClick = onClick,
+            icon = icon,
+            enabled = enabled,
+            colors = colors,
+            borderColor = borderColor,
+            modifier = modifier.height(40.dp),
+        )
+    }
+
+    @Composable
+    fun VerticalOutlined(
+        text: String,
+        onClick: () -> Unit,
+        icon: ImageVector? = null,
+        enabled: Boolean = true,
+        colors: ButtonColors = ButtonColors(
+            containerColor = MaterialTheme.colorScheme.onPrimary,
+            contentColor = MaterialTheme.colorScheme.primary,
+            disabledContentColor = MaterialTheme.colorScheme.outline,
+            disabledContainerColor = MaterialTheme.colorScheme.onPrimary,
+        ),
+        borderColor: Color? = null,
+        modifier: Modifier = Modifier,
+    ) {
+        VerticalOutlinedButtonBase(
             text = text,
             onClick = onClick,
             icon = icon,
@@ -103,6 +154,38 @@ object BankButton {
     }
 
     @Composable
+    private fun VerticalButtonBase(
+        text: String,
+        onClick: () -> Unit,
+        colors: ButtonColors,
+        icon: ImageVector? = null,
+        enabled: Boolean = true,
+        modifier: Modifier = Modifier,
+    ) {
+        Button(
+            modifier = modifier,
+            onClick = onClick,
+            shape = RoundedCornerShape(20.dp),
+            colors = colors,
+            enabled = enabled,
+        ) {
+            Column {
+                icon?.let { icon ->
+                    Icon(
+                        modifier = Modifier.size(18.dp),
+                        imageVector = icon,
+                        contentDescription = null,
+                    )
+                }
+                Text(
+                    text = text,
+                    style = S14_W500,
+                )
+            }
+        }
+    }
+
+    @Composable
     private fun OutlinedButtonBase(
         text: String,
         onClick: () -> Unit,
@@ -134,6 +217,42 @@ object BankButton {
                 text = text,
                 style = S14_W500,
             )
+        }
+    }
+
+    @Composable
+    private fun VerticalOutlinedButtonBase(
+        text: String,
+        onClick: () -> Unit,
+        colors: ButtonColors,
+        icon: ImageVector? = null,
+        enabled: Boolean = true,
+        borderColor: Color? = null,
+        modifier: Modifier = Modifier,
+    ) {
+        OutlinedButton(
+            modifier = modifier,
+            onClick = onClick,
+            shape = RoundedCornerShape(20.dp),
+            colors = colors,
+            enabled = enabled,
+            border = borderColor?.let {
+                    color -> BorderStroke(width = 1.dp, color = color)
+            } ?: ButtonDefaults.outlinedButtonBorder,
+        ) {
+            Column {
+                icon?.let { icon ->
+                    Icon(
+                        modifier = Modifier.size(18.dp),
+                        imageVector = icon,
+                        contentDescription = null,
+                    )
+                }
+                Text(
+                    text = text,
+                    style = S14_W500,
+                )
+            }
         }
     }
 }
